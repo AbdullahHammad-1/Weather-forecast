cfda8b5f7b33c241fafaca55939e540862cd036a
Abdallah Hammad <abdallahhammad440@gmail.com>
2024-09-20 01:11:55 +0300

    Initial commit

---
+++ backend.py
@@ -0,0 +1,17 @@
+import requests
+
+API_KEY = "2613e7bda2f4c05ffd830a2336bf6523"
+
+
+def get_data(place, forcast_days=None, kind=None):
+    url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
+    response = requests.get(url)
+    data = response.json()
+    filtered_data = data["list"]
+    nr_values = 8 * forcast_days
+    filtered_data = filtered_data[:nr_values]
+    return filtered_data
+
+
+if __name__ == "__main__":
+    print(get_data(place="Tokyo"))
